5e65b8d1a0379cb9f0a30f2910fdd0d116509ae4
dzavi84 <milanvidanovic@yahoo.com>
2022-03-16 15:36:47 +0100

    commit1

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import './App.css';
 import { MyButton } from './MyButton';
+export interface IAppProps {}
+export interface IAppState {
+  showDetails: boolean;
+}
 
-class App extends React.Component {
+class App extends React.Component<IAppProps, IAppState> {
+  constructor(props: IAppProps) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { showDetails: false };
+  }
   render() {
     return (
-      <MyButton
-        buttonName='Click Here'
-        handleButtonClick={this.handleClick}></MyButton>
+      <div>
+        <p>showDetails={this.state.showDetails ? 'true' : 'false'}</p>
+        <MyButton
+          buttonName='Click Here'
+          handleButtonClick={this.handleClick}></MyButton>
+      </div>
     );
   }
   handleClick() {
     console.log(`App.handleClick() called`);
+    this.setState({
+      showDetails: !this.state.showDetails,
+    });
   }
 }
 export default App;
