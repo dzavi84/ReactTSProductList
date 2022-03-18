031e2a4bfc1193e4916d61317d791a92eedf4367
dzavi84 <milanvidanovic@yahoo.com>
2022-03-18 18:20:38 +0100

    commitLast

--- src/App.tsx
+++ src/App.tsx
@@ -4,37 +4,72 @@ import './App.css';
 import { MyButton } from './MyButton';
 import { Collection, IProduct } from './Products';
 import { DetailView } from './DetailView';
+import { ajax } from 'rxjs/ajax';
+import { CircularProgress } from '@material-ui/core';
+import { MicroEventBus } from 'MicroFrontEnd';
 export interface IAppProps {}
+
+export let microEventBus: MicroEventBus = (window as any).microEventBus;
 export interface IAppState {
   showDetails: boolean;
   product: IProduct | null;
+  collectionInstance: Collection;
+  username: string;
 }
 
-const collectionInstance = new Collection();
+//const collectionInstance = new Collection();
 class App extends React.Component<IAppProps, IAppState> {
   constructor(props: IAppProps) {
     super(props);
     this.showDetailView = this.showDetailView.bind(this);
     this.handleClose = this.handleClose.bind(this);
-    this.state = { showDetails: false, product: null };
+    this.state = {
+      showDetails: false,
+      product: null,
+      collectionInstance: new Collection(),
+      username: '',
+    };
+  }
+  componentDidMount() {
+    this.processUserLoginEvent = this.processUserLoginEvent.bind(this);
+    microEventBus.on('user-logged-in').subscribe(this.processUserLoginEvent);
+    ajax.getJSON('http://128.0.0.1:3000/products').subscribe((data) => {
+      let collection = new Collection();
+      collection.items = data as IProduct[];
+      this.setState({
+        showDetails: false,
+        product: null,
+        collectionInstance: collection,
+      });
+    });
   }
   render() {
     return (
       <div>
+        {this.state.collectionInstance.items.length < 1 ? (
+          <div className='App-header'>
+            <CircularProgress />
+          </div>
+        ) : (
+          ''
+        )}
         <CollectionView
-          {...collectionInstance}
+          {...this.state.collectionInstance}
           handlerItemClicked={this.showDetailView}></CollectionView>
         <DetailView
           open={this.state.showDetails}
           product={this.state.product}
-          handleClose={this.handleClose}></DetailView>
+          handleClose={this.handleClose}
+          username={this.state.username}></DetailView>
       </div>
     );
   }
   showDetailView(id: number) {
-    let foundItem = collectionInstance.items.find((item: IProduct) => {
-      return item.id === id;
-    });
+    let foundItem = this.state.collectionInstance.items.find(
+      (item: IProduct) => {
+        return item.id === id;
+      },
+    );
     if (foundItem) {
       this.setState({
         showDetails: true,
@@ -48,6 +83,12 @@ class App extends React.Component<IAppProps, IAppState> {
       showDetails: false,
       product: null,
     });
+  }
+  processUserLoginEvent(event: string | undefined) {
+    console.log(`RCT:App.processUserLoginEvent:${event}`);
+    if (event) {
+      this.setState({ username: event });
+    }
   }
 }
 
--- src/Products.tsx
+++ src/Products.tsx
@@ -16,52 +16,6 @@ export interface ISpecs {
   price: string;
 }
 export class Collection {
-  items: IProduct[] = [
-    {
-      id: 1,
-      name: 'Holy Panda',
-      type: 'Tactile',
-      image:
-        'https://ih0.redbubble.net/image.618638614.1485/raf,750x1000,075,t,fafafa:ca443f4786.jpg',
-      longDescription: '...',
-      specs: {
-        actuationForce: '44',
-        actuationPoint: '2.4',
-        bottomOut: '62',
-        bottomOutTravel: '3.8',
-        price: '1.60',
-      },
-    },
-    {
-      id: 2,
-      name: 'Ball',
-      type: 'Tactile',
-      image:
-        'https://www.dhresource.com/0x0s/f2-albu-g10-M01-37-44-rBVaVlwXTHGAcrxyAAG7z5SOYSU760.jpg/9-5-oversize-giant-tennis-ball-for-children.jpg',
-      longDescription: '...',
-      specs: {
-        actuationForce: '44',
-        actuationPoint: '2.4',
-        bottomOut: '62',
-        bottomOutTravel: '3.8',
-        price: '5.60',
-      },
-    },
-    {
-      id: 3,
-      name: 'Ring',
-      type: 'Tactile',
-      image:
-        'https://cbu01.alicdn.com/img/ibank/2018/917/621/10207126719_1492657371.jpg',
-      longDescription: '...',
-      specs: {
-        actuationForce: '44',
-        actuationPoint: '2.4',
-        bottomOut: '62',
-        bottomOutTravel: '3.8',
-        price: '11.60',
-      },
-    },
-  ];
+  items: IProduct[] = [];
   handlerItemClicked = (_id: number) => {};
 }
